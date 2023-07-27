4233c76b6aa27aebcd8b0a698754bfcf433efde7
杨可可 <yangkeke@dmtop1.com>
2023-07-27 11:21:58 +0800

    remove react

---
+++ example/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// import QuickStart from './QuickStart';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    {/* <QuickStart></QuickStart> */}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
